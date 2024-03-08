437dd653b4942c414c9ac4562af204588f874c3f
Flylink <F.orch@mail.ru>
2024-03-08 16:33:46 +0700

    add JUnit Jupiter test

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -2,6 +2,7 @@ package ru.netology.service;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 
 public class CashbackHackServiceTest {
 
@@ -36,5 +37,38 @@ public class CashbackHackServiceTest {
         int actual = service.remain(amount);
 
         Assert.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testJupiterRemainShouldReturn100WhenAmountIs900() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+        int expected = 100;
+
+        int actual = service.remain(amount);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testJupiterRemainShouldReturn500WhenAmountIs1500() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1500;
+        int expected = 500;
+
+        int actual = service.remain(amount);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testJupiterRemainShouldReturn0WhenAmountIs1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int expected = 0;
+
+        int actual = service.remain(amount);
+
+        Assertions.assertEquals(expected, actual);
     }
 }
